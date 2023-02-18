53c5aba612c16cd30435f2c17db31fb53897dccf
Jay_Lai <jay1415jay1415@yahoo.com.tw>
2023-02-18 12:24:40 +0800

    商品功能 - Refactor categor，使用Enum類型

--- final/src/main/java/com/jaylai/springbootmall/model/Product.java
+++ final/src/main/java/com/jaylai/springbootmall/model/Product.java
@@ -1,6 +1,7 @@
 package com.jaylai.springbootmall.model;
 
-import java.sql.Timestamp;
+import com.jaylai.springbootmall.constant.ProductCategory;
+
 import java.util.Date;
 
 public class Product {
@@ -8,7 +9,7 @@ public class Product {
 
     private Integer productId;
     private String productName;
-    private String category;
+    private ProductCategory category;
     private String imageUrl;
     private Integer price;
     private Integer stock;
@@ -32,11 +33,11 @@ public class Product {
         this.productName = productName;
     }
 
-    public String getCategory() {
+    public ProductCategory getCategory() {
         return category;
     }
 
-    public void setCategory(String category) {
+    public void setCategory(ProductCategory category) {
         this.category = category;
     }
 
